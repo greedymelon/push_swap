123d4c79f4a2618220eeef734e800d6c5290597d
Debora Monfrini <dmonfrin@f1r2s3.codam.nl>
2022-08-17 16:03:02 +0200

    tring out

--- stack_sort.c
+++ stack_sort.c
@@ -6,7 +6,7 @@
 /*   By: dmonfrin <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/25 13:31:49 by dmonfrin      #+#    #+#                 */
-/*   Updated: 2022/08/17 11:30:21 by dmonfrin      ########   odam.nl         */
+/*   Updated: 2022/08/17 15:58:46 by dmonfrin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,27 @@ int	is_ascending (t_stack *a)
 	return (1);
 }
 
-int solve_puzzle (t_stack **a, int stack_element)
+void	solve_puzzle (t_stack **a, int numb_element)
 {
 	t_stack *b;
 
+	b = NULL;
 	if (is_ascending(*a))
-		return (1);
-	if (stack_element <= 5)
-	{}
-	if (stack_element < 500)
-	{}
+		return ;
+	if (numb_element <= 3)
+		three_algo(a, &b);
+	if (numb_element <= 5)
+		five_algo(a, &b);
+	if (numb_element > 5)
+		undred_algo(a, &b);
+	free_stack(&b);
 	return(1);
+}
+
+void three_algo(t_stack **a, t_stack **b)
+{
+	if (((*a)->nb > (*a)->next->nb) && ((*a)->nb < (*a)->next->next->nb))
+		compute_move(pa, a, &b);
+	if (((*a)->nb < (*a)->next->nb) && ((*a)->nb > (*a)->next->next->nb))
+		compute_move(ra, a, &b);
 }
